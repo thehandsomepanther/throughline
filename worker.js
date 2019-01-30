79129d6bf6cde2ad7575ef855f526ccdcbedc093
Josh Shi <thehandsomepanther@gmail.com>
2019-01-29 21:24:42 -0800

    Updates

---
+++ worker.js
@@ -0,0 +1,11 @@
+onmessage = (e) => {
+  let data = e.data[0];
+
+  let workerResult = null;
+  (() => {
+    var e = null; // remove "e"
+    workerResult = eval(data);
+  })();
+
+  self.postMessage(JSON.stringify(workerResult));
+};
